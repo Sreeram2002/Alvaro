5d7352fe34b5b68da4c98bb3b40dd63b0258f017
Sreeram2002 <75155661+Sreeram2002@users.noreply.github.com>
2021-11-28 19:56:47 +0530

    Create find location with python.py

---
+++ find location with python.py
@@ -0,0 +1,16 @@
+
+# importing geopy library
+from geopy.geocoders import Nominatim
+  
+# calling the Nominatim tool
+loc = Nominatim(user_agent="GetLoc")
+  
+# entering the location name
+getLoc = loc.geocode("Gosainganj Lucknow")
+  
+# printing address
+print(getLoc.address)
+  
+# printing latitude and longitude
+print("Latitude = ", location.latitude, "\n")
+print("Longitude = ", location.longitude)
